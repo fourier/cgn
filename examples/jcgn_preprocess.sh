071ffcafb66efe043765a219e336ba372ee27f72
Alexey Veretennikov <alexey.veretennikov@gmail.com>
2015-02-13 08:53:59 +0100

    updated scripts

--- examples/jcgn_preprocess.sh
+++ examples/jcgn_preprocess.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 SAXON_PATH=~/Development/SaxonHE9-6-0-4J
-if [ "$1" == "1" ] || [ "$1" == "2" ] || [ "$1" == "3" ] || [ "$1" == "4" ] || [ "$1" == "5" ] || [ "$1" == "6" ]; then
+if [ ! -z $1 ] && [ "$1" -ge "1" ] && [ "$1" -le "6" ]; then
     java -cp $SAXON_PATH/saxon9he.jar net.sf.saxon.Transform -s:example.xml -xsl:../objects/java/java_prepobjects_main.xsl -im:"{https://github.com/fourier/cgn/java}preprocess$1" -strip:all \!indent=yes
 else 
     java -cp $SAXON_PATH/saxon9he.jar net.sf.saxon.Transform -s:example.xml -xsl:../objects/java/java_prepobjects_main.xsl -im:"{https://github.com/fourier/cgn/java}preprocess" -strip:all \!indent=yes
