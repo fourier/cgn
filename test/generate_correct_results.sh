11c0594c7f80fab7c0670d068576e31d94825311
Alexey Veretennikov <alexey.veretennikov@gmail.com>
2015-02-13 10:10:32 +0100

    Test cgn:phase2 done

--- test/generate_correct_results.sh
+++ test/generate_correct_results.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 ../tools/cgn_preprocess.sh cgn_phase1_test.xml 1 > cgn_phase1_test_expected.xml
+../tools/cgn_preprocess.sh cgn_phase2_test.xml 2 > cgn_phase2_test_expected.xml
--- test/run_tests.sh
+++ test/run_tests.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
+echo Testing cgn:phase1
 ../tools/cgn_preprocess.sh cgn_phase1_test.xml 1 > cgn_phase1_test_result.xml
 diff -q cgn_phase1_test_result.xml cgn_phase1_test_expected.xml
+echo Testing cgn:phase2
+../tools/cgn_preprocess.sh cgn_phase2_test.xml 2 > cgn_phase2_test_result.xml
+diff -q cgn_phase2_test_result.xml cgn_phase2_test_expected.xml
