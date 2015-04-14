199ec4d1f0cecd4f702db15691e82d68459be146
Alexey Veretennikov <alexey.veretennikov@gmail.com>
2015-04-14 17:07:07 +0200

    Refactoring tests

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -1,21 +1,2 @@
 #!/bin/sh
-set -e
-CGN_PHASES=5
-for i in $(seq 1 $CGN_PHASES); do
-    TEST_FILE=cgn_phase${i}_test.xml
-    RESULT_FILE=cgn_phase${i}_test_result.xml
-    EXPECT_FILE=cgn_phase${i}_test_expected.xml
-    echo Testing cgn:phase$i
-    ../tools/cgn_preprocess.sh $TEST_FILE $i > $RESULT_FILE
-    diff -q $RESULT_FILE $EXPECT_FILE
-done
-
-JCGN_PHASES=7
-for i in $(seq 1 $JCGN_PHASES); do
-    TEST_FILE=jcgn_phase${i}_test.xml
-    RESULT_FILE=jcgn_phase${i}_test_result.xml
-    EXPECT_FILE=jcgn_phase${i}_test_expected.xml
-    echo Testing jcgn:phase$i
-    ../tools/jcgn_preprocess.sh $TEST_FILE $i > $RESULT_FILE
-    diff -q $RESULT_FILE $EXPECT_FILE
-done
+python ../ctt/ctt.py preprocess_tests.xml
--- tools/cgn_preprocess.sh
+++ tools/cgn_preprocess.sh
@@ -5,8 +5,9 @@ if [ -z $1 ]; then
     echo "Usage: $0 testfile.xml [1..4]"
     exit 1
 fi
+OUTPUT="$(dirname $1)/$(basename $1 .xml)_result.xml"
 if [ ! -z $2 ] && [ "$2" -ge "1" ] && [ "$2" -le "4" ]; then
-    java -cp $SAXON_PATH/saxon9he.jar net.sf.saxon.Transform -s:$1 -xsl:$CURRENT_PATH/../objects/cgn_prepobjects_main.xsl -im:"{https://github.com/fourier/cgn}preprocess$2" -strip:all \!indent=yes
+    java -cp $SAXON_PATH/saxon9he.jar net.sf.saxon.Transform -s:$1 -xsl:$CURRENT_PATH/../objects/cgn_prepobjects_main.xsl -im:"{https://github.com/fourier/cgn}preprocess$2" -strip:all \!indent=yes > $OUTPUT
 else 
-    java -cp $SAXON_PATH/saxon9he.jar net.sf.saxon.Transform -s:$1 -xsl:$CURRENT_PATH/../objects/cgn_prepobjects_main.xsl -im:"{https://github.com/fourier/cgn}preprocess" -strip:all \!indent=yes          
+    java -cp $SAXON_PATH/saxon9he.jar net.sf.saxon.Transform -s:$1 -xsl:$CURRENT_PATH/../objects/cgn_prepobjects_main.xsl -im:"{https://github.com/fourier/cgn}preprocess" -strip:all \!indent=yes > $OUTPUT          
 fi
