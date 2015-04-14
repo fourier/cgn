4745e8a74709f9984b645ef4e1eb4d786569c098
Alexey Veretennikov <alexey.veretennikov@gmail.com>
2015-04-14 20:14:55 +0200

    Updated ctt - it is now supports the stdout results; updated tests according to the latest ctt changes

--- tools/jcgn_preprocess.sh
+++ tools/jcgn_preprocess.sh
@@ -4,11 +4,11 @@ if [ -z $1 ]; then
     echo "Usage: $0 testfile.xml [1..6]"
     exit 1
 fi
-OUTPUT="$(dirname $1)/$(basename $1 .xml)_result.xml"
+
 if [ ! -z $2 ] && [ "$2" -ge "1" ] && [ "$2" -le "6" ]; then
-    java -cp $SAXON_PATH/saxon9he.jar net.sf.saxon.Transform -s:$1 -xsl:../objects/java/java_prepobjects_main.xsl -im:"{https://github.com/fourier/cgn/java}preprocess$2" -strip:all \!indent=yes > $OUTPUT
+    java -cp $SAXON_PATH/saxon9he.jar net.sf.saxon.Transform -s:$1 -xsl:../objects/java/java_prepobjects_main.xsl -im:"{https://github.com/fourier/cgn/java}preprocess$2" -strip:all \!indent=yes
 else 
-    java -cp $SAXON_PATH/saxon9he.jar net.sf.saxon.Transform -s:$1 -xsl:../objects/java/java_prepobjects_main.xsl -im:"{https://github.com/fourier/cgn/java}preprocess" -strip:all \!indent=yes > $OUTPUT
+    java -cp $SAXON_PATH/saxon9he.jar net.sf.saxon.Transform -s:$1 -xsl:../objects/java/java_prepobjects_main.xsl -im:"{https://github.com/fourier/cgn/java}preprocess" -strip:all \!indent=yes 
 fi
 
 
