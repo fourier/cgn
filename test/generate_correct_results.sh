b79b43ae5380432e2704d21e21e449f5afb9a381
Alexey Veretennikov <txm.fourier@gmail.com>
2015-06-15 21:08:52 +0200

    Updated script generating correct results

--- test/generate_correct_results.sh
+++ test/generate_correct_results.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
 EXPECTED_DIR=expected
+INPUT_DIR=input_data
 CGN_PHASES=5
 for i in $(seq 1 $CGN_PHASES); do
-    TEST_FILE=cgn_phase${i}_test.xml
+    TEST_FILE=$INPUT_DIR/cgn_phase${i}_test.xml
     RESULT_FILE=$EXPECTED_DIR/cgn_phase${i}_test_expected.xml
     ../tools/cgn_preprocess.sh $TEST_FILE $i > $RESULT_FILE
 done
 
 # type counts test
-../tools/cgn_run_template.sh cgn_type_counts_test.xsl create-type-counts-xml-test cgn_type_counts_test.xml > $EXPECTED_DIR/cgn_type_counts_test_expected.xml
+../tools/cgn_run_template.sh cgn_type_counts_test.xsl create-type-counts-xml-test $INPUT_DIR/cgn_type_counts_test.xml > $EXPECTED_DIR/cgn_type_counts_test_expected.xml
 
 JCGN_PHASES=7
 for i in $(seq 1 $JCGN_PHASES); do
-    TEST_FILE=jcgn_phase${i}_test.xml
+    TEST_FILE=$INPUT_DIR/jcgn_phase${i}_test.xml
     RESULT_FILE=$EXPECTED_DIR/jcgn_phase${i}_test_expected.xml
     ../tools/jcgn_preprocess.sh $TEST_FILE $i > $RESULT_FILE
 done
