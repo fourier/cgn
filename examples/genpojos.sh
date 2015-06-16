b87e08789a43b05eebef14abaf7e92c7588207f4
Alexey Veretennikov <txm.fourier@gmail.com>
2015-06-16 19:28:36 +0200

    Added environment variable setting to scripts

--- examples/genpojos.sh
+++ examples/genpojos.sh
@@ -3,7 +3,8 @@
 DEFAULT_SAXON_PATH=~/Development/SaxonHE9-6-0-4J
 SAXON_PATH=${SAXON_PATH-$DEFAULT_SAXON_PATH}
 
-if [ "$1" -eq "-d" ]; then
+if [ "$1" == "-d" ]
+then
   java -cp $SAXON_PATH/saxon9he.jar net.sf.saxon.Transform -T -s:example.xml -xsl:example_main.xsl -it:"{https://github.com/fourier/cgn/java}genobjects"
 else
   java -cp $SAXON_PATH/saxon9he.jar net.sf.saxon.Transform -s:example.xml -xsl:example_main.xsl -it:"{https://github.com/fourier/cgn/java}genobjects"
--- test/gen_java_objects_parser.sh
+++ test/gen_java_objects_parser.sh
@@ -1,5 +1,8 @@
 #! /bin/sh
-SAXON_PATH=~/Development/SaxonHE9-6-0-4J
+# using default saxon path if SAXON_PATH variable not defined
+DEFAULT_SAXON_PATH=~/Development/SaxonHE9-6-0-4J
+SAXON_PATH=${SAXON_PATH-$DEFAULT_SAXON_PATH}
+
 java -cp $SAXON_PATH/saxon9he.jar net.sf.saxon.Transform -s:$1 -xsl:java_objects_parser.xsl -it:"{https://github.com/fourier/cgn/java}gen-parser-main"
 #-o:../../dummy.xml
 
